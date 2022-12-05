990ab59aad7eb626a493c80f13593e41c333cd68
rob <92820434+robiboo@users.noreply.github.com>
2022-12-05 01:02:14 +0000

    Finalizing code

--- README.md
+++ README.md
@@ -6,6 +6,7 @@ Vivian Duong, Emily Lim, Robert Panerio
 The project is based on a tutorial competition from Kaggle called Bag of Words Meets Bags of Popcorn. The competition focuses on using Word2Vec for sentiment analysis and deep learning. 
 
 Our project focuses on a part of the competition where we take a dataset and apply a Bag of Words to get accurate predictions whether the a review is thumbs up or thumbs down. 
+
 	
 ### Libraries Used:
 1. Sklearn
@@ -27,3 +28,23 @@ Our project focuses on a part of the competition where we take a dataset and app
 
 ### How to run the program:
 1. python3 main.py
+
+### Prompts
+The user will first be asked to select what type of estimator they would like to use: 
+1. logistic regressor (l)
+2. random forest classifier (r)
+
+Next they can pick what they would like to happen with the run 
+1. Demo (d) is the version that will be used in class for the presentation. The user will
+   be asked to enter in a detailed movie review and the program will guess (using training
+   data from kaggle) whether they liked the movie or not. (~1 min)
+3. Kaggle (k) this uses the given training and test sets for the kaggle competition.
+   Outputs the a file of predictions that can be submitted to Kaggle (~1 min).
+5. Train test (t) uses only the training data and creates a train test split to get an
+   accuracy score without submitting to kaggle (~1 min).
+7. Logistic regression with optimization grid (o) will run with optimizations using RandomSearchCV.
+   NOTE: we have already populated the results of best_params in the other modes so running any other version
+   will yield the optimized results (~30 - 60 min).
+9. Random forest with optimization grid (o) will run random forest with optimizations using RandomSearchCV.
+   NOTE: we have already populated the results of best_params in the other modes so running any other version
+   will yield the optimized results (~35 min). 
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 # from sklearn.linear_model import LogisticClassification
-from sklearn.model_selection import RandomizedSearchCV
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
 import pandas as pd
 import numpy as np
 import sklearn
@@ -13,6 +13,8 @@ from sklearn.model_selection import train_test_split
 from sklearn import metrics
 from sklearn.ensemble import RandomForestClassifier
 from scipy.stats import randint
+from sklearn.preprocessing import Normalizer
+import time 
 
 # Clean up the reviews data
 class Preprocessor(BaseEstimator, TransformerMixin):
@@ -46,75 +48,124 @@ class Preprocessor(BaseEstimator, TransformerMixin):
 #     return int( sentiment == 'positive' )
 
 def get_review():
-    review = input("Please give a review: " )
-    print(review)
+    review = input("Please give a movie review. Tell us what you thought of the plot, acting, cinematography, etc. The more the better! " )
+    # print(review)
     return review
 
+def normalize_sentiment(sentiment):
+    return int(sentiment == 'positive')
+def demo(train, steps):
+    xs = train[['review']]  # Training reviews
+    ys = train['sentiment']  # Traing target
+    user_review = get_review()
+    d = {'review': [user_review]}
+    df = pd.DataFrame(data=d)
+    # print(df[['review']])
+    x_test = df[['review']]
+    pipe = Pipeline(steps)
+    pipe.fit(xs, ys)
+    prediction = pipe.predict(x_test)
+    if prediction[0] == 0:
+        print("You hated this movie!")
+    else:
+        print("you loved this movie <3")
+def train_test(data, steps):
+    # split data into train and test
+    train = data.sample(frac=0.7)
+    test = data.drop(train.index)
+    print(test)
+    xs = train[['review']]  # Training reviews
+    ys = train['sentiment']  # Training target
+
+    x_test = test[['review']]
+    print(x_test)
+    y_test = test['sentiment']
+    pipe = Pipeline(steps)
+    pipe.fit(xs, ys)
+    prediction = pipe.predict(x_test)
+    score = metrics.accuracy_score(y_test, prediction)
+
+    print("accuracy is: ", score)
+
+def kaggle_prediction(train, test, steps):
+    xs = train[['review']]  # Training reviews
+    ys = train['sentiment']  # Traing target
+
+    x_test = test[['review']]
+    pipe = Pipeline(steps)
+    pipe.fit(xs, ys)
+    prediction = pipe.predict(x_test)
+    print(prediction)
+
+    output = pd.DataFrame(data={'id': test['id'], "sentiment": prediction})
+    output.to_csv("Bag_of_Words_model.csv", index=False, quoting=3)
+
+
+def logistic_regression_grid(train, steps):
+    xs = train[['review']]  # Training reviews
+    ys = train['sentiment']  # Traing target
+    pipe = Pipeline(steps)
+    grid_logistic = {
+        'classify__C' : [1e-5, 1e-3, 1e-1, 1e0, 1e1, 1e2],
+        # {'classify' : [RandomForestClassifier()],
+        # 'classify__n_estimators' : list(range(10,101,10)),
+        # 'classify__max_features' : list(range(6,32,5))}
+    }
+    search = RandomizedSearchCV(pipe, grid_logistic, scoring='accuracy', n_jobs=-1)
+    search.fit(xs, ys)
+
+    print(search.best_score_) # r-squared score
+    print(search.best_params_) # hyperparameters
+
+def random_forest_grid(train, steps):
+    xs = train[['review']]  # Training reviews
+    ys = train['sentiment']  # Traing target
+    pipe = Pipeline(steps)
+    grid_regression = {
+        'classify__n_estimators' : list(range(10,101,10)),
+        'classify__max_features' : list(range(6,32,5))
+    }
+    search = RandomizedSearchCV(pipe, grid_regression, scoring='accuracy', n_jobs=-1)
+    search.fit(xs, ys)
+
+    print(search.best_score_) # r-squared score
+    print(search.best_params_) # hyperparameters
+
 def main():
-
-    # imdb = pd.read_csv("IMDB Dataset.csv")
-    # print("imdb length", len(imdb))
-    #
-    # imdb['sentiment'] = imdb['sentiment'].apply(normalize_sentiment)
-
+  
     # All training data
     train = pd.read_csv("labeledTrainData.tsv", header=0, \
                         delimiter="\t", quoting=3)
-
-    print("train length", len(train))
-    #
-    # frames = [train, imdb]
-    #
-    # train = pd.concat(frames)
-    # print("new train length", len(train))
 
     # All test data
     test = pd.read_csv("testData.tsv", header=0, delimiter="\t", \
                        quoting=3)
 
-    print("test length", len(test))
-    # print(data.head)
-    # split data into train and test
-    # train_data = train.sample(frac=0.7)
-    # test = train.drop(train.index)
+    # IMDB dataset for the demo
+    imdb = pd.read_csv("IMDB Dataset.csv", header=0)
+    imdb['sentiment'] = imdb['sentiment'].apply(normalize_sentiment)
 
-    #
-    xs = train[['review']]   # Training reviews
-    ys = train['sentiment']  # Traing target
+    estimator = input("Please put in \'l\' for logistic regression or \'r\' for random forest (default is l): ")
 
-    x_test = test[['review']]
-    # print(xs)
-    user_review = get_review()
-    # d = {'review': [user_review]}
-    # df = pd.DataFrame(data=d)
-    # print(df[['review']])
-    # x_test = df[['review']]
-    x_test = test[['review']]
-    # y_test = test['sentiment']
-
-    # xs = data[['review']]
-    # ys = data['sentiment']
-
-    # print(xs)
-    # print(ys)
-
-    # test preprocessor outside of pipeline to see if it works
-    # t = Preprocessor()
-    # xs_lower = t.transform(xs)
-    # print(xs_lower)
+    if(estimator != 'l' and estimator != 'r'):
+        print("Sorry you have given the wrong estimator!!")
+        return
+    
+    mode = input("Which type of experiment would you like to do? Options are \ndemo (d), \nkaggle (k), \ntrain test (t), \
+    \nOptimization (o): ")
 
     # vectorize the train data for logistic regression
-    # vectorizer = \
-    #     CountVectorizer(
-    #         max_df=0.1,
-    #         min_df=2,
-    #         strip_accents='unicode',
-    #         max_features=100000,
-    #         ngram_range=(1, 3),
-    #     )
+    vectorizer_l = \
+        CountVectorizer(
+            max_df=0.1,
+            min_df=2,
+            strip_accents='unicode',
+            max_features=100000,
+            ngram_range=(1, 3),
+        )
 
     # vectorizer for random forest
-    vectorizer = \
+    vectorizer_r = \
         CountVectorizer(
             max_df=0.1,
             min_df=5,
@@ -122,74 +173,50 @@ def main():
             max_features=100000,
             ngram_range=(1, 3),
         )
-    # steps
-    # steps = [
-    #     ('tokenize', Preprocessor()),
-    #     ('vectorize', vectorizer),
-    #     ('classify', LogisticRegression(solver='lbfgs', max_iter=1000))
-    # ]
 
-    steps = [
+    vectorizer = vectorizer_l
+  
+    if estimator == 'r':
+        vectorizer = vectorizer_r
+    
+
+    steps_l = [
         ('tokenize', Preprocessor()),
         ('vectorize', vectorizer),
-        ('classify', RandomForestClassifier())
+        ('normalize', Normalizer( norm = 'l1' ) ),
+        ('classify', LogisticRegression(solver='lbfgs', max_iter=1000, C=100))
     ]
 
-    # Grid for vectorizor
-    grid = {
-        # 'vectorize__ngram_range': [(1, 1), (1, 3), (2, 3)],
-        # 'vectorize__max_df': [1.0, 0.7, 0.1],
-        # 'vectorize__min_df': [2, 5, 10, 100],
-        # 'vectorize__max_features': [1000, 10000, 100000],
-        'classify__max_depth': list(np.arange(10, 100, step=10)) + [None],
-        "classify__n_estimators": np.arange(10, 500, step=50),
-        'classify__max_features': randint(1, 7),
-        'classify__criterion': ['gini', 'entropy'],
-        'classify__min_samples_leaf': randint(1, 4),
-        'classify__min_samples_split': np.arange(2, 10, step=2)
-    }
+    steps_r = [
+        ('tokenize', Preprocessor()),
+        ('vectorize', vectorizer),
+        ('normalize', Normalizer( norm = 'l1' ) ),
+        ('classify', RandomForestClassifier(n_estimators=100, max_features=26))
+    ]
+    steps = steps_l
+    if ( estimator == 'r'):
+        steps = steps_r
+    
 
-    # Grid for random forest
-    # rs_space = {'max_depth': list(np.arange(10, 100, step=10)) + [None],
-    #             'n_estimators': np.arange(10, 500, step=50),
-    #             'max_features': randint(1, 7),
-    #             'criterion': ['gini', 'entropy'],
-    #             'min_samples_leaf': randint(1, 4),
-    #             'min_samples_split': np.arange(2, 10, step=2)
-    #             }
+    if (mode == 'd'):
+        print("Demo version")
+        demo(imdb, steps)
+    elif (mode == 'k'):
+        print('Submit Bag_of_Words_model.csv to Kaggle to get accuracy score')
+        kaggle_prediction(train, test, steps)
+    elif (mode == 't'):
+        print('Sample train test split')
+        train_test(train, steps)
+    elif (mode == 'o' and estimator != 'r'):
+        print('Logistic Regression optimization with Random Search CV')
+        logistic_regression_grid(train, steps)
+    elif (mode == 'o' and estimator == 'r'):
+        print('Random Forest Classifier optimization with Random Search CV')
+        random_forest_grid(train , steps)
+    else:
+        print('Sorry you have given the wrong mode!!')
+    return
+  
 
-
-    pipe = Pipeline(steps)
-    pipe.fit(xs, ys)
-    prediction = pipe.predict(x_test)
-
-    print(prediction)
-
-    # print(list(y_test))
-
-
-    # score = metrics.accuracy_score(y_test, prediction)
-
-    # print("accuracy is: ", score)
-
-    # pass in test reviews to classify
-    # print(pipe.predict_proba([['Thought this movie was very cool.'], ['Thought this movie was lame.']]))
-
-    # Comment in for the
-    search = RandomizedSearchCV(pipe, grid, scoring='accuracy', n_jobs=-1)
-    search.fit(xs, ys)
-
-    print(search.best_score_) # r-squared score
-    print(search.best_params_) # hyperparameters
-
-
-    # grid_forest = RandomizedSearchCV(pipe,rs_space,scoring='accuracy')
-    # grid_forest.fit(xs, ys)
-    #
-    # print(grid_forest.best_score_)  # r-squared score
-    # print(grid_forest.best_params_)  # hyperparameters
-
-    output = pd.DataFrame(data={'id': test['id'], "sentiment": prediction})
-    output.to_csv("Baf_of_Words_model.csv", index=False, quoting=3)
 if __name__ == "__main__":
     main()
